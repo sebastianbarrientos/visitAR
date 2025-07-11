cf0aa92454e5ccb00ab07d5429b9dfbd04c7eb58
sebastianbarrientos <sba04012@gmail.com>
2025-07-11 16:02:43 -0300

    Seleccion de idiomas, nuevos estilos y página en inglés

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ window.onscroll = function () {
 goUpBtn.addEventListener("click", e => {
     window.scrollTo({top: 0, behavior: "smooth"})
 })
+
+function changeLanguage(select) {
+    const selectedFile = select.value;
+    console.log("Idioma seleccionado:", selectedFile);
+
+    if (selectedFile == "en") {
+        window.location.href = "./home_en.html";
+    } else if (selectedFile == "es") {
+        window.location.href = "./home.html";
+    }
+  }
+
+
+function toggleLanguage(language) {
+    if (language == "en") {
+        window.location.href = "./home_en.html";
+    } else if (language == "es") {
+        window.location.href = "./home.html";
+    }
+  }
